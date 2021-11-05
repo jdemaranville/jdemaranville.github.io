3f25d048c4bb598910eefcac17092333f2d790f6
jdemaranville <jdemaranville@centriq.com>
2021-11-05 08:21:23 -0500

    Initial Commit

---
+++ jdemaranville.github.io/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jdemaranville.github.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
